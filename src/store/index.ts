bbbdaad36581b2bd209505dc5e2b0a300c99d3e6
1000ch <shogosensui@gmail.com>
2020-06-29 17:31:31 +0900

    refactor: use module mode

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,10 @@
-import { createStore } from 'vuex-smart-module';
-import { rootModule } from './modules/root';
+import { Module } from 'vuex-smart-module';
+import { authModule } from './modules/auth';
 
-export default () => createStore(rootModule, {
-  strict: false
+export const root = new Module({
+  modules: {
+    auth: authModule
+  }
 });
+
+export const { state, getters, mutations, actions, modules, plugins } = root.getStoreOptions();
